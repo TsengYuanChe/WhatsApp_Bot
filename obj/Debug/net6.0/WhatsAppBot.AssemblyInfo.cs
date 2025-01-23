146c13576409bbd266d8ddabf8dba9da24ee48c4
曾源喆 <adam880614@gmail.com>
2025-01-23 13:09:54 +0800

    Initial commit

--- obj/Debug/net6.0/WhatsAppBot.AssemblyInfo.cs
+++ obj/Debug/net6.0/WhatsAppBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhatsAppBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b77706cdf4ebfb8d2afee3e9a0e0ceb525e599d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3f6f70d07747f844922ed31f4b6430d4090da3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhatsAppBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhatsAppBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
